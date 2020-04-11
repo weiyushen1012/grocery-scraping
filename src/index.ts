88343e2a2bee5874ddc26bba8d686956e9475418
Weiyu Shen <weiyushen1012@gmail.com>
2020-04-11 18:03:06 -0400

    add script for scraping jet

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,28 @@
 import logger from "./logging";
 import puppeteer, { Browser, Page } from "puppeteer";
+import jet from "./scripts/jet";
 
-const address: string = "https://jet.com/";
-const keyword: string = "clorox toilet wand refills";
+const HEADLESS: boolean = true;
 
 const main = async (): Promise<void> => {
+    logger.info("********************");
     logger.info("Start scraping");
 
     logger.info("Creating headless browser");
-    const browser: Browser = await puppeteer.launch({ headless: false });
+    const browser: Browser = await puppeteer.launch({ headless: HEADLESS });
 
-    const page: Page = await browser.newPage();
-    page.setViewport({ height: 1000, width: 1000 });
-    page.goto(address);
+    browser.on("targetdestroyed", async () => {
+        const pages: Page[] = await browser.pages();
 
-    page.once("load", async () => {
-        await page.waitFor(1000);
-
-        await page.click("#MobileSearchBarInput");
-
-        await page.waitFor(1000);
-
-        await page.keyboard.type("clorox toilet wand refills");
-
-        await page.waitFor(1000);
-        await page.keyboard.press("Enter");
-        await page.waitFor(2000);
-
-        const hits = await page.$$(".fSSCaC");
-
-        if (hits && hits.length > 0) {
-            await page.screenshot({
-                path: `${keyword.split(" ").join("_")}.jpeg`,
-            });
+        if (pages.length <= 1 && browser.isConnected()) {
+            logger.info("Terminating headless browser");
+            logger.info("********************");
+            browser.close();
         }
-
-        await page.waitFor(3000);
-
-        logger.info("Terminating headless browser");
-        browser.close();
     });
+
+    logger.info("Scraping Jet");
+    await jet(browser);
 };
 
 main();
---
+++ src/scripts/jet.ts
@@ -0,0 +1,61 @@
+import { Page, Browser } from "puppeteer";
+import logger from "../logging";
+
+const ADDRESS: string = "https://jet.com/";
+const SEARCH_BAR_HTML_ID = "MobileSearchBarInput";
+
+const keywords: string[] = ["clorox toilet wand refills"];
+
+const scrapingKeyword = async (page: Page, keyword: string): Promise<void> => {
+    logger.info(`Searching: ${keyword}`);
+    await page.keyboard.type(keyword);
+
+    await page.keyboard.press("Enter");
+
+    await page.waitForNavigation();
+
+    const hits = await page.$$(".fSSCaC");
+
+    if (hits && hits.length > 0) {
+        const imageFileName = `${keyword.split(" ").join("_")}.jpeg`;
+        logger.info(
+            `Got ${hits.length} hits, taking screenshot ${imageFileName}`
+        );
+        await page.screenshot({
+            path: imageFileName,
+        });
+    } else {
+        logger.info(`No hit`);
+    }
+};
+
+const scraping = async (browser: Browser): Promise<void> => {
+    const page: Page = await browser.newPage();
+    await page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    );
+
+    page.setViewport({ height: 1000, width: 1000 });
+
+    logger.info(`Loading address ${ADDRESS}`);
+    await page.goto(ADDRESS);
+
+    logger.info(`Clicking search bar with id ${SEARCH_BAR_HTML_ID}`);
+    await page.click(`#${SEARCH_BAR_HTML_ID}`);
+
+    logger.info(`Searching for all keywords`);
+
+    const promises: Promise<void>[] = [];
+    keywords.forEach(
+        async (keyword: string): Promise<void> => {
+            promises.push(scrapingKeyword(page, keyword));
+        }
+    );
+
+    await Promise.all(promises);
+
+    logger.info(`Closing page for Jet`);
+    await page.close();
+};
+
+export default scraping;
