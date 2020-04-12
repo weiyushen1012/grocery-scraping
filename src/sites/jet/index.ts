6040534847912abdacdca2a01a2b5a42631680fd
Weiyu Shen <weiyushen1012@gmail.com>
2020-04-12 03:18:06 -0400

    add search query string for jet

--- src/sites/jet/index.ts
+++ src/sites/jet/index.ts
@@ -5,51 +5,8 @@ import fs = require("fs");
 import keywords from "./keywords";
 
 const ADDRESS: string = "https://jet.com/";
-const SEARCH_BAR_HTML_ID = "MobileSearchBarInput";
 const HIT_HTML_CLASS = "fSSCaC";
 const OUT_OF_STOCK_HTML_CLASS = "eIFaPZ";
-
-const scrapingKeyword = async (page: Page, keyword: string): Promise<void> => {
-    logger.info(`Searching: ${keyword}`);
-    await page.keyboard.type(keyword);
-    await page.keyboard.press("Enter");
-    await page.waitForNavigation();
-
-    const hits = await page.$$(`.${HIT_HTML_CLASS}`);
-    const outOfStock = await page.$$(`.${OUT_OF_STOCK_HTML_CLASS}`);
-
-    const hitsNumber = hits ? hits.length : 0;
-    const outOfStockNumber = outOfStock ? outOfStock.length : 0;
-
-    if (hitsNumber > 0 && hitsNumber > outOfStockNumber) {
-        const imageFileName: string = `${keyword.split(" ").join("_")}.jpeg`;
-        logger.info(
-            `Got ${
-                hitsNumber - outOfStockNumber
-            } hits, taking screenshot [${imageFileName}]`
-        );
-
-        const directoryPath: string = path.resolve(
-            __dirname,
-            "../../..",
-            "screenshots"
-        );
-
-        if (!fs.existsSync(directoryPath)) {
-            fs.mkdirSync(directoryPath);
-        }
-
-        await page.screenshot({
-            path: path.resolve(directoryPath, imageFileName),
-        });
-    } else {
-        if (hitsNumber === 0) {
-            logger.info(`No hit: bad searching phrase`);
-        } else {
-            logger.info(`No hit: out of stock`);
-        }
-    }
-};
 
 const scraping = async (browser: Browser): Promise<void> => {
     const page: Page = await browser.newPage();
@@ -59,9 +16,49 @@ const scraping = async (browser: Browser): Promise<void> => {
     );
 
     for (let i = 0; i < keywords.length; i++) {
-        await page.goto(ADDRESS);
-        await page.click(`#${SEARCH_BAR_HTML_ID}`);
-        await scrapingKeyword(page, keywords[i]);
+        await page.goto(`${ADDRESS}search?term=${keywords[i]}`);
+        const keyword = keywords[i];
+
+        logger.info(`Searching: ${keyword}`);
+
+        await page.waitFor(5000);
+
+        const hits = await page.$$(`.${HIT_HTML_CLASS}`);
+        const outOfStock = await page.$$(`.${OUT_OF_STOCK_HTML_CLASS}`);
+
+        const hitsNumber = hits ? hits.length : 0;
+        const outOfStockNumber = outOfStock ? outOfStock.length : 0;
+
+        if (hitsNumber > 0 && hitsNumber > outOfStockNumber) {
+            const imageFileName: string = `${keyword
+                .split(" ")
+                .join("_")}.jpeg`;
+            logger.info(
+                `Got ${
+                    hitsNumber - outOfStockNumber
+                } hits, taking screenshot [${imageFileName}]`
+            );
+
+            const directoryPath: string = path.resolve(
+                __dirname,
+                "../../..",
+                "screenshots"
+            );
+
+            if (!fs.existsSync(directoryPath)) {
+                fs.mkdirSync(directoryPath);
+            }
+
+            await page.screenshot({
+                path: path.resolve(directoryPath, imageFileName),
+            });
+        } else {
+            if (hitsNumber === 0) {
+                logger.info(`No hit: bad searching phrase`);
+            } else {
+                logger.info(`No hit: out of stock`);
+            }
+        }
     }
 
     await page.close();
