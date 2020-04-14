a1968309c6976055ce7d83615c2cc9150cf95f21
Weiyu Shen <weiyushen1012@gmail.com>
2020-04-13 22:47:38 -0400

    ready for deployment

--- dist/email/mailList.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mailList = [
-    "weiyushen1012@gmail.com",
-    "chavezmago@yahoo.com",
-];
-//# sourceMappingURL=mailList.js.map
--- dist/sites/jet/keywords.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const keywords = [
-    "scott toilet paper rolls",
-    "clorox toilet wand refills",
-];
-exports.default = keywords;
-//# sourceMappingURL=keywords.js.map
